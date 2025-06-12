da22b683e62718aac8dcd2558da753b9ebe854e1
Toby Gore <tobyedwardgore@gmail.com>
2025-06-12 20:16:47 +0100

    updates all round

--- src/examples/stars/scripts.ts
+++ src/examples/stars/scripts.ts
@@ -1,50 +1,76 @@
-"use strict";
 class RandomCoordinates {
+    x: string;
+    y: string;
+
     constructor() {
         this.x = Math.random() * 100 + '%';
         this.y = Math.random() * 100 + '%';
     }
 }
+
+
 class ShootingStar {
+    star: HTMLDivElement;
+    start: RandomCoordinates;
+    
     constructor() {
         this.star = document.createElement('div');
         this.star.classList.add('star');
+
         this.start = new RandomCoordinates();
+
         this.setStyles();
     }
+
     setStyles() {
         this.setNewStyleVar('--start-x', this.start.x);
         this.setNewStyleVar('--start-y', this.start.y);
-        this.setNewStyleVar('--duration', Math.random() * 6 + 5 + 's');
+
+        this.setNewStyleVar('--duration', Math.random() * 6 + 5 + 's'); 
+
         this.setNewStyleVar('--delay', Math.random() + 's');
+
         this.setNewStyleVar('--top', Math.random() * 100 - 30 + '%');
-        this.setNewStyleVar('--left', Math.random() * 100 - 30 + '%');
+        this.setNewStyleVar('--left', Math.random() * 100 - 30+ '%');
+
         this.setNewStyleVar('--size', (Math.random() + 0.5).toString());
+
         this.setNewStyleVar('--angle', Math.random() * 360 + 'deg');
     }
-    setNewStyleVar(styleName, styleValue) {
+
+    setNewStyleVar(styleName: string, styleValue: string) {
         this.star.style.setProperty(styleName, styleValue);
     }
+
     getStar() {
         return this.star;
     }
 }
+
 new ShootingStar();
+
 class NightSky {
+    nightSky: HTMLDivElement;
+    numberOfStars: number;
+
     constructor() {
-        var _a;
         const nightSky = document.getElementById('night-sky');
+
         if (!nightSky) {
-            throw Error("No night sky element found");
+            throw Error("No night sky element found")
         }
-        this.nightSky = nightSky;
-        this.numberOfStars = Number.parseInt((_a = this.nightSky.style.getPropertyValue('--number-of-stars')) !== null && _a !== void 0 ? _a : '10');
+
+        this.nightSky = nightSky as HTMLDivElement;
+
+        this.numberOfStars = Number.parseInt(this.nightSky.style.getPropertyValue('--number-of-stars') ?? '10');
         this.addStars(this.numberOfStars);
     }
-    addStars(numberOfStars) {
+
+    addStars(numberOfStars: number) {
         for (let i = 0; i < numberOfStars; i++) {
             this.nightSky.appendChild(new ShootingStar().getStar());
         }
     }
 }
+
 new NightSky();
